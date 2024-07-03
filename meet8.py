50499e10673eb29b3542102e982841d7635207ab
Sistem operasi <166475447+dindadamayantiputri@users.noreply.github.com>
2024-07-03 11:41:49 +0800

    Add files via upload

---
+++ meet8.py
@@ -0,0 +1,24 @@
+#percabangan
+nama = input("silahkan masukkan nama :")
+
+#if kondisi : aksi (standar)
+if nama == "edy" : print("dosen baik")
+print ("program selesai")
+
+#if indentasi
+if nama == "edy" :
+    print("dosen baik")
+    print("dosen baik bgt")
+    print(f"{nama} baik")
+print ("program selesai")
+
+#if else
+if nama == "edy" :
+    print("dosen baik")
+    print("dosen baik bgt")
+else :
+    print("baik aja")
+    print("kamu bukan edy")
+
+print ("program selesai")
+print(f"{nama} baik")
