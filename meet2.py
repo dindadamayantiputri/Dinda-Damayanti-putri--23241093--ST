cacd1591e2c4a26fc5c5f2b570ecfa30d8f8dfcb
Sistem operasi <166475447+dindadamayantiputri@users.noreply.github.com>
2024-07-03 11:42:42 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,13 @@
+#Ini pembelajaran variabel
+d = 9
+i = 4
+azhari = 3
+kata10 = 2
+
+print("nilai saya adalah =", d)
+print("nilai saya adalah =", i)
+
+#Menjumlahkan Variabel x dan y
+print("d =", d)
+print("i =", i)
+print("d + i =", d + i)
