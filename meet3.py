e02007560054e742bbb8b60fc6ee5c19caaa32b1
Sistem operasi <166475447+dindadamayantiputri@users.noreply.github.com>
2024-07-03 11:41:08 +0800

    Add files via upload

---
+++ meet3.py
@@ -0,0 +1,13 @@
+# belajar tipe data
+
+#tipe data integer
+bil_integer = 9
+print ("Nilai bil_integer =", bil_integer, "jenis tipe datanya =" , type(bil_integer))
+
+#tipe data float
+bil_float = 3.5
+print ("Nilai bil_float =", bil_float, "jenis tipe datanya =" , type(bil_float))
+
+#tipe data string (kumpulan karakter)
+data_string = "my name is azhari"
+print ("Nilai data_string =", data_string, "jenis tipe datanya =" , type(data_string))
